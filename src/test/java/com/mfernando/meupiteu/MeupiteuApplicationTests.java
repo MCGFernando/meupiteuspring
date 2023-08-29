5fe5a9027604fe722ae544380ed15324556a8a01
MCGFernando <marofernando1991@gmail.com>
2023-08-30 00:42:08 +0100

    Project created

---
+++ src/test/java/com/mfernando/meupiteu/MeupiteuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfernando.meupiteu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeupiteuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
